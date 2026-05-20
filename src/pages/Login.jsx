6cf0170670c33aac4fa3f97d6537b40ec84df1ad
hieuhp132 <hieuhp132@gmail.com>
2026-05-20 23:17:11 +0700

    c

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch("https://paintball-raising-military-topic.trycloudflare.com/api/v1/auth/login", {
+      const response = await fetch("http://103.72.98.176:3000/api/v1/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
